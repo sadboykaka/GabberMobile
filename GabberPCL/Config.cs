f7118acb7eb767fafc6a7a80a8e62eaddfca99bb
Jay Rainey <jawrainey@gmail.com>
2018-05-21 00:09:48 +0100

    :rocket: First iOS IFRC release

--- GabberPCL/Config.cs
+++ GabberPCL/Config.cs
@@ -2,8 +2,8 @@
 {
     public static class Config
     {
-        public static string WEB_URL = "https://gabber.audio";
-        public static string API_ENDPOINT = "https://api.dev.gabber.audio";
+        public static string WEB_URL = "https://ifrc.gabber.audio";
+        public static string API_ENDPOINT = "https://api.ifrc.gabber.audio";
         public static string DATABASE_NAME = "gabber.db3";
     }
 }
